58e51c71d5758ba112a2314a50fdd799813d5841
Xiao Lin <linxiao9292@outlook.com>
2016-10-27 15:44:53 +0800

    The first draft of SFC Maple APP

---
+++ M_BRO/impl/src/main/java/org/opendaylight/mapleapp/impl/MapleappProvider.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright © 2015 SNLAB and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mapleapp.impl;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.sal.binding.api.NotificationProviderService;
+import org.opendaylight.maple.core.increment.MapleCore;
+import org.opendaylight.maple.core.increment.app.MapleAppBase;
+import org.opendaylight.maple.core.increment.packet.Ethernet;
+import org.opendaylight.maple.core.increment.tracetree.*;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
+import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
+import org.opendaylight.maple.impl.ConfigFlowManager.FlowManagerWX;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.service.rev130819.SalFlowService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketProcessingListener;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketProcessingService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketReceived;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.NotificationListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+
+public class MapleappProvider implements BindingAwareProvider, AutoCloseable, PacketProcessingListener {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MapleappProvider.class);
+
+    private PacketProcessingService pps;
+
+    private SalFlowService fs;
+
+    private ListenerRegistration<NotificationListener> notificationListenerReg;
+
+    MapleCore mapleCore;
+
+    DataBroker db;
+
+    //M3 mapleApp;
+
+
+    MapleAppBase topMapleApp;
+
+    FlowManagerWX fmwx;
+
+    String chain;
+
+
+    public MapleappProvider(String chain) {
+        this.chain = chain;
+    }
+
+
+    @Override
+    public void onSessionInitiated(ProviderContext session) {
+        this.mapleCore = MapleCore.allocateMapleCore();
+        if (chain.contains(",")){
+            String[] apps = chain.split(",");
+            for (int i = apps.length - 1; i >= 0; i--) {
+                String name = apps[i];
+                try {
+                    MapleAppBase ap = (MapleAppBase) Class.forName(name).newInstance();
+                    this.topMapleApp = ap;
+                } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                System.out.println("maple app name: " + name);
+            }
+        } else {
+            MapleAppBase ap = null;
+            try {
+                ap = (MapleAppBase) Class.forName("org.opendaylight.mapleapp.impl." + chain).newInstance();
+            } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            this.topMapleApp = ap;
+        }
+        LOG.info("MapleappProvider Session Initiated");
+        pps = session.getRpcService(PacketProcessingService.class);
+        db = session.getSALService(DataBroker.class);
+        fmwx = new FlowManagerWX(db, pps);
+        if(this.mapleCore == null) {
+            LOG.info("MapleCore is empty in External Mapleapp");
+        }
+        NotificationProviderService nps = session.getSALService(NotificationProviderService.class);
+        this.notificationListenerReg = nps.registerNotificationListener(this);
+    }
+
+    @Override
+    public void close() throws Exception {
+        LOG.info("MapleappProvider Closed");
+    }
+
+    @Override
+    public void onPacketReceived(PacketReceived packetReceived) {
+        LOG.info("External Mapleapp onPacketRecieved is invorked");
+        byte[] payload = packetReceived.getPayload();
+
+        Ethernet frame = new Ethernet();
+
+        frame.deserialize(payload, 0, payload.length);
+
+
+        if (frame.getEtherType() == Ethernet.TYPE_ARP || frame.getEtherType() == Ethernet.TYPE_IPv4) {
+
+            String tpIdString = fmwx.getTpIdFromNodeConnectorRef(packetReceived.getIngress()).getValue(); // "openFlow:1:1"
+
+            System.out.println("tpIdString: " + tpIdString);
+
+            Port port = new Port(tpIdString);
+
+            MaplePacket maplePkt = new MaplePacket(frame, port);
+
+            String pktHash = maplePkt.toString();
+
+            System.out.println("hash: " + pktHash);
+
+            this.mapleCore.getPktHash2PayLoadPort().put(pktHash
+                    , new PayLoadAndPort(payload, new Port(tpIdString)));
+
+            topMapleApp.preWork(maplePkt);
+
+            topMapleApp.onPacket(maplePkt);
+
+            Trace trace = new Trace();
+
+            List<TraceItem> compressedItemList = Trace.compress(maplePkt.itemList);
+
+            for (TraceItem ti : compressedItemList) {
+                trace.addTraceItem(ti, pktHash);
+            }
+
+            //for (TraceItem ti : maplePkt.itemList) {
+            //    trace.addTraceItem(ti, pktHash);
+            //}
+            trace.addTraceItem(maplePkt.getInstruction().toItem(), pktHash);
+
+            fmwx.sendPacket(payload, port, maplePkt.route());
+
+            //LOG.info("lastNode in trace: " + trace.lastNode.toString());
+
+            mapleCore.updateTrace(pktHash, trace);
+
+            LOG.info("finish update trace");
+        }
+    }
+}
---
+++ M_BRO/impl/src/main/java/org/opendaylight/mapleapp/impl/SimpleSFC.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright (c) 2016 SNLAB and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mapleapp.impl;
+
+import org.opendaylight.maple.core.increment.app.MapleAppBase;
+import org.opendaylight.maple.core.increment.app.MapleUtil;
+import org.opendaylight.maple.core.increment.packet.Ethernet;
+import org.opendaylight.maple.core.increment.packet.IPv4;
+import org.opendaylight.maple.core.increment.tracetree.MaplePacket;
+import org.opendaylight.maple.core.increment.tracetree.Port;
+import org.opendaylight.maple.core.increment.tracetree.Route;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+public class SimpleSFC extends MapleAppBase {
+
+	private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(SimpleSFC.class);
+
+	private static final String TOPO_URL       = "/root/network-topology/topology";
+	private static final String HOST_TABLE_URL = "/root/host-table";
+	private static final String FLOW_FILE_NAME_URL  = "/root/flow-file-name";
+
+	private static final String      H1    = "192.168.0.11";
+	private static final int H1_IP = IPv4.toIPv4Address(H1);
+
+	private static final String      H3    = "192.168.0.13";
+	private static final int H3_IP = IPv4.toIPv4Address(H3);
+
+	private static final String      H_BRO = "192.168.0.15";
+	private static final int HBRO_IP = IPv4.toIPv4Address(H_BRO);
+
+	private static final String[] H13_FAST_PATH = { "openflow:966034607844:3", "openflow:966034607668:2", "openflow:949187772416:1" };
+	private static final String[] H13_SLOW_PATH  = { "openflow:966034607844:4", "openflow:949187772416:1" };
+	private static final String[] H31_FAST_PATH = { "openflow:949187772416:4", "openflow:966034607668:1", "openflow:966034607844:1" };
+	private static final String[] H31_SLOW_PATH  = { "openflow:949187772416:3", "openflow:966034607844:1" };
+
+	private String flow2Filename(FlowId flowId) {
+
+		return readData(FLOW_FILE_NAME_URL).get(FlowId);
+
+	}
+
+	private void forwardToBro(MaplePacket pkt) {
+
+		// Get information for computing the path between src and Bro
+		Map<Integer, Port> hostTable = (Map<Integer, Port>) readData(HOST_TABLE_URL);
+		int srcIP = pkt.IPv4Src();
+		Port srcPort = hostTable.get(srcIP);
+		Port dstPort = hostTable.get(HBRO_IP);
+		Topology topo = (Topology) readData(TOPO_URL);
+
+		// Compute the path, and send the packet to the BRO Server
+		pkt.addRoute(MapleUtil.shortestPath(topo.getLink(), srcPort, dstPort));
+
+	}
+
+	public void onPacket(MaplePacket pkt) {
+
+		int ethType = pkt.ethType();
+
+		// For IPv4 traffic only
+		if ( ethType == Ethernet.TYPE_IPv4 ) {
+
+
+			if ( pkt.IPv4SrcIs(H1_IP) && pkt.IPv4DstIs(H3_IP) ) {
+
+				// TODO: Need to decide the key of the mapping between flows and filenames
+				String fn = flow2Filename(pkt.flow);
+
+				if ( fn == null ) {
+
+					// TODO: Need to know this packet is the starting point of a transfer
+					if ( pkt includes the HTTP GET request ) {
+						forwardToBro(pkt);
+					}
+
+					// At the same time, send the packet to the origin destination
+					// TODO: This flow may need hard time out. So we can shift this flow to the fast path
+					pkt.addRoute(H13_SLOW_PATH);
+
+				}
+
+				// Suppose all science big files begin with "SC_"
+				// A second pakcet-in may be needed, so that the flow on the slow path can be shifted in to the fast
+				// path.
+				else if ( fn.startsWith("SC_") ) {
+
+					pkt.addRoute(H13_FAST_PATH);
+
+				} else {
+
+						pkt.addRoute(H13_SLOW_PATH);
+
+				}
+
+			} else if ( pkt.IPv4SrcIs(H3_IP) && pkt.IPv4DstIs(H1_IP)) {
+
+				// TODO: Need to know what path this packet belongs to, fast path or slow path
+				if ( pkt belongs to FAST_PATH ) {
+					pkt.addRoute(H31_FAST_PATH);
+				}
+				else {
+					pkt.addRoute(H31_SLOW_PATH);
+				}
+
+			} else {
+
+				pkt.addRoute(Route.Drop);
+
+			}
+		}
+
+		else {
+
+			passToNext(pkt);
+
+		}
+	}
+}
+
+
+
